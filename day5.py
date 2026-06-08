2b72d1281d95ba06650f14c90c441ca81b13b16f
Pablo Ortega <pablo_ortega@es.ibm.com>
2026-06-08 11:02:16 +0200

    Day 5 part 2 completed

--- day5.py
+++ day5.py
@@ -6,10 +6,10 @@ def hasEnoughVocals(chain):
     return i_vocals >= 3
 
 def hasLetterDuplicated(chain):
-    s_chain = list(chain)
+    l_chain = list(chain)
     
-    for i in range(0, len(s_chain) - 1):
-        if s_chain[i] == s_chain[i + 1]:
+    for i in range(0, len(l_chain) - 1):
+        if l_chain[i] == l_chain[i + 1]:
             return True
     
     return False
@@ -41,5 +41,43 @@ def number5_1():
         
     print("Result day 5 part 1: The number of nice strings is " + str(nice_strings))
     
+# Test if a pair of letters appears at least two times without overlapping (aaa is overlap)
+def hasPairOfLetters(chain):
+    l_line = list(chain)
+    
+    for i in range(0, len(l_line) - 2):
+        pair = "".join(l_line[i:i+2])
+        if pair in "".join(l_line[i+2:]):
+            return True
+    
+    return False
+
+# Test if a letter is repeated but with one letter in the middle without any restriction about what letter is in the middle
+def isLetterRepeated(chain):
+    l_line = list(chain)
+    
+    for i in range(0, len(l_line) - 2):
+        if l_line[i] == l_line[i+2]:
+            return True
+    
+    return False
+    
+def number5_2():
+    dataFile = open("data/day5.txt", "r")
+    
+    nice_strings = 0
+    for line in dataFile:
+        s_line = line.replace("\n", "")
+        
+        if not hasPairOfLetters(s_line):
+            continue
+        if not isLetterRepeated(s_line):
+            continue
+        
+        nice_strings += 1
+        
+    print("Result day 5 part 2: The number of nice strings are " + str(nice_strings))
+    
     
 number5_1()
+number5_2()
