38ece453f9c48bcee965f2fc6d375539cad5ff34
Pablo Ortega <pablo_ortega@es.ibm.com>
2026-06-09 16:36:11 +0200

    Day 9 completed

--- day9.py
+++ day9.py
@@ -14,18 +14,17 @@ def dijskstra_worst(start, graph, current_distance):
             if city in current_path and len(current_path) > 1: # Going back or the loop is completed
                 # Testing if the path is good
                 if len(current_path) == len(graph.keys()): # All the cities in the path
-                    if best_distance == -1 or path_distance > best_distance:
+                    if best_distance == -1 or path_distance > best_distance: 
                         best_distance = path_distance
             else: # the city is a new one in the path
                 aux_path = current_path.copy()
                 aux_path.append(city)
                 aux_distance = path_distance + graph[current_city][city]   
-                if current_distance == -1 or aux_distance > current_distance: # the path is still better
-                    heapq.heappush(queue, (city, aux_distance, aux_path))
+                heapq.heappush(queue, (city, aux_distance, aux_path))
     
     if current_distance == -1:
         return best_distance
-    elif current_distance > best_distance:
+    elif current_distance < best_distance:
         return best_distance
     else:
         return -1
@@ -86,7 +85,7 @@ def number9_1():
         if res != -1:
             current_path_distance = res
         
-    print("Result day 9 part 2: The longest path is " + str(current_path_distance))
+    print("Result day 9 part 1: The shortest path is " + str(current_path_distance))
     
 def number9_2():
     dataFile = open("data/day9.txt", "r")
@@ -113,7 +112,7 @@ def number9_2():
         if res != -1:
             current_path_distance = res
         
-    print("Result day 9 part 1: The sortest path is " + str(current_path_distance))
+    print("Result day 9 part 2: The longest path is " + str(current_path_distance))
     
 number9_1()
 number9_2()
